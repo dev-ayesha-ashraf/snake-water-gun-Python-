ba654ad50c089e3f13295aa5e16c15f1eb2e7a3e
Ayesha Ashraf <ayeshaashraf0999@gmail.com>
2025-03-01 13:36:07 -0700

    pythonFirstProject

---
+++ Game.py
@@ -0,0 +1,20 @@
+computer = "snake"
+you = input("Enter Your Choice: ")
+print(f"computer chose {computer} and you chose {you}")
+if(computer == you):
+    print("It's a Draw")
+else:
+    if(computer == "snake" and you == "water"):
+        print("you Lose")
+    elif(computer == "water" and you == "snake"):
+        print("You won")
+    elif(computer == "water" and you == "gun"):
+        print("You Lose")
+    elif(computer == "gun" and you == "water"):
+        print("You Won")
+    elif(computer == "gun" and you == "snake"):
+        print("You Lose")
+    elif(computer == "snake" and you == "gun"):
+        print("You won")
+    else:
+        print("something went wrong")
